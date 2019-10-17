4e373d0440f9b0ea5ac4399428c48fee4565b1bb
Alejandro Balderas <alejandro@hotmail.de>
2019-10-17 21:32:22 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require("firebase");
+require("firebase/firestore");
+var firebaseConfig = {
+  apiKey: "AIzaSyCJKQz7bREVhUZC8sVPnpqHeT1k18EcCVs",
+  authDomain: "evernote-clone-8fa30.firebaseapp.com",
+  databaseURL: "https://evernote-clone-8fa30.firebaseio.com",
+  projectId: "evernote-clone-8fa30",
+  storageBucket: "evernote-clone-8fa30.appspot.com",
+  messagingSenderId: "529093418223",
+  appId: "1:529093418223:web:533afc182b01efc3228f1a",
+  measurementId: "G-939B8CTV0V"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
